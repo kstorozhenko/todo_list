15ab6df3af46f5670f208f63107e721d18b37981
Kyrylo Storozhenko <busterakel@gmail.com>
2024-03-11 12:56:46 +0200

    add todo views to urls and make base todo page

--- app_todo/urls.py
+++ app_todo/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 
-from app_todo.views import HomeListView
+from app_todo.views import HomeListView, TagListView, TodoCreateView, TodoUpdateView, TodoDeleteView
 
 urlpatterns = [
-    path("", HomeListView.as_view(), name="home")
-
+    path("", HomeListView.as_view(), name="home"),
+    path("tags/", TagListView.as_view(), name="tags"),
+    path("todo/create/", TodoCreateView.as_view(), name="todo_create"),
+    path("todo/<int:pk>/update/", TodoUpdateView.as_view(), name="todo_update"),
+    path("todo/<int:pk>/delete/", TodoDeleteView.as_view(), name="todo_delete"),
 ]
